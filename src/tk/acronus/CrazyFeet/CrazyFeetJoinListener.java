734d2a012fc4a2bebd70d436b29c6366cd04a23d
runescapejon <jonsimpletv@gmail.com>
2016-04-09 18:51:27 -0400

    Added files via upload

--- src/tk/acronus/CrazyFeet/CrazyFeetJoinListener.java
+++ src/tk/acronus/CrazyFeet/CrazyFeetJoinListener.java
@@ -8,13 +8,19 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import tk.acronus.CrazyFeet.Util.Files.CrazyAutoFireFile;
-import tk.acronus.CrazyFeet.Util.Files.CrazyAutoFireHeadFile;
 import tk.acronus.CrazyFeet.Util.Files.CrazyAutoMagicFile;
 import tk.acronus.CrazyFeet.Util.Files.CrazyAutoPearlFile;
 import tk.acronus.CrazyFeet.Util.Files.CrazyAutoSmokeFile;
 import tk.acronus.CrazyFeet.Util.Files.CrazyAutonoteFile;
 import tk.acronus.CrazyFeet.Util.Files.CrazyAutoWitchFile;
 import tk.acronus.CrazyFeet.Util.Files.CrazyAutoHeartFile;
+import tk.acronus.CrazyFeet.Util.Files.CrazyAutoFireHeadFile;
+import tk.acronus.CrazyFeet.Util.Files.CrazyAutoHeartHeadFile;
+import tk.acronus.CrazyFeet.Util.Files.CrazyAutoMagicHeadFile;
+import tk.acronus.CrazyFeet.Util.Files.CrazyAutoNoteHeadFile;
+import tk.acronus.CrazyFeet.Util.Files.CrazyAutoWitchHeadFile;
+import tk.acronus.CrazyFeet.Util.Files.CrazyAutoSmokeHeadFile;
+import tk.acronus.CrazyFeet.Util.Files.CrazyAutoPearlHeadFile;
 
 
 public class CrazyFeetJoinListener implements Listener {
@@ -27,6 +33,72 @@ public class CrazyFeetJoinListener implements Listener {
 		if(CrazyAutoFireFile.cFPlayers.contains(player.getName())) {
 			CrazyFeet.CrazyFire.add(player);
 			player.sendMessage(green+"You have joined the game with automatic CrazyFire. To disable this, type /crazyautofire");
+		} else {
+			//doNothing
+		}
+	}
+
+	@EventHandler
+	public void onCrazyHeartHeadJoin(PlayerJoinEvent pJE) {	
+		Player player = pJE.getPlayer();	
+		if(CrazyAutoHeartHeadFile.cHHPlayers.contains(player.getName())) {
+			CrazyFeet.CrazyHeartHead.add(player);
+			player.sendMessage(green+"You have joined the game with automatic CrazyheartHead. To disable this, type /crazyautohearthead");
+		} else {
+			//doNothing
+		}
+	}
+
+	@EventHandler
+	public void onCrazyPearlHeadJoin(PlayerJoinEvent pJE) {	
+		Player player = pJE.getPlayer();	
+		if(CrazyAutoPearlHeadFile.cPHPlayers.contains(player.getName())) {
+			CrazyFeet.CrazyPearlHead.add(player);
+			player.sendMessage(green+"You have joined the game with automatic CrazyPaerlHead. To disable this, type /crazyautopearlhead");
+		} else {
+			//doNothing
+		}
+	}
+	
+	@EventHandler
+	public void onCrazyMagicHeadJoin(PlayerJoinEvent pJE) {	
+		Player player = pJE.getPlayer();	
+		if(CrazyAutoMagicHeadFile.cMHPlayers.contains(player.getName())) {
+			CrazyFeet.CrazyMagicHead.add(player);
+			player.sendMessage(green+"You have joined the game with automatic CrazyMagicHead. To disable this, type /crazyautomagichead");
+		} else {
+			//doNothing
+		}
+	}
+
+	@EventHandler
+	public void onCrazyNoteHeadJoin(PlayerJoinEvent pJE) {	
+		Player player = pJE.getPlayer();	
+		if(CrazyAutoNoteHeadFile.cNHPlayers.contains(player.getName())) {
+			CrazyFeet.CrazyNoteHead.add(player);
+			player.sendMessage(green+"You have joined the game with automatic CrazyNoteHead. To disable this, type /crazyautonotehead");
+		} else {
+			//doNothing
+		}
+	}
+	
+	@EventHandler
+	public void onCrazySmokeHeadJoin(PlayerJoinEvent pJE) {	
+		Player player = pJE.getPlayer();	
+		if(CrazyAutoSmokeHeadFile.cSHPlayers.contains(player.getName())) {
+			CrazyFeet.CrazySmokeHead.add(player);
+			player.sendMessage(green+"You have joined the game with automatic CrazySmokeHead. To disable this, type /crazyautosmokehead");
+		} else {
+			//doNothing
+		}
+	}
+	
+	@EventHandler
+	public void onCrazyWitchHeadJoin(PlayerJoinEvent pJE) {	
+		Player player = pJE.getPlayer();	
+		if(CrazyAutoWitchHeadFile.cWHPlayers.contains(player.getName())) {
+			CrazyFeet.CrazyWitchHead.add(player);
+			player.sendMessage(green+"You have joined the game with automatic CrazywitchHead. To disable this, type /crazyautowitchhead");
 		} else {
 			//doNothing
 		}
